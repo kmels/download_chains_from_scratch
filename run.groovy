bfbb47c59ff0d103a7cf1e6978074cde9571612b
Carlos Lopez <c.lopez@kmels.net>
2018-07-24 22:04:36 +0000

    update to 0.14.7-bip47.4

--- run.groovy
+++ run.groovy
@@ -2,40 +2,46 @@
 this.getClass().classLoader.rootLoader.addURL(new File("./bitcoincashj-core-0.14.5-bip47-bundled.jar").toURL())
 
 
-import org.bitcoinj.core.Utils
-import org.bitcoinj.params.BCCMainNetParams
-import org.bitcoinj.params.BCCTestNet3Params
-import org.bitcoinj.params.TestNet3Params
-import org.bitcoinj.wallet.bip47.Blockchain
-import org.bitcoinj.wallet.bip47.Wallet;
-import org.bitcoinj.params.MainNetParams
+import org.bitcoinj.core.*
+import org.bitcoinj.params.*;
+import org.bitcoinj.kits.*
 import org.bitcoinj.wallet.bip47.listeners.BlockchainDownloadProgressTracker
-import org.bitcoinj.wallet.bip47.models.StashDeterministicSeed
+
+import org.bitcoinj.wallet.DeterministicSeed;
+import org.bitcoinj.crypto.*;
 
 import javax.annotation.Nullable;
 pct = 0
 
 println("BLOCKCHAIN: " + args[0]);
 
-StashDeterministicSeed seed ;
+def seed;
 if (args.length > 1) {
     String mnemonic = args[1..args.length-1].join(" ");
     println("SEED: ${mnemonic}")
-    seed = new StashDeterministicSeed(mnemonic, "", Utils.currentTimeSeconds());
+
+    bip39bornDate = MnemonicCode.BIP39_STANDARDISATION_TIME_SECS;
+    seed = new DeterministicSeed(mnemonic, null, "", bip39bornDate);
 }
 
-blockchains = [new Blockchain(2,BCCMainNetParams.get(), "BCH", "Bitcoin Cash"),
-               new Blockchain(3, BCCTestNet3Params.get(), "tBCH", "Test Bitcoin Cash"),
-               new Blockchain(0, MainNetParams.get(), "BTC", "Bitcoin Core"),
-               new Blockchain(1, TestNet3Params.get(), "tBTC", "Test Bitcoin Core")]
+def blockchains = [
+    	       // "BCH": BCCMainNetParams.get(),
+               // "tBCH": BCCTestNet3Params.get(),
+               "BTC": MainNetParams.get(),
+               "tBTC": TestNet3Params.get()]
 
-Blockchain blockchain = blockchains.find { it.coin == args[0]}
+def params = blockchains.get(args[0])
+
 dir = new File(".");
-coin = blockchain.coin
+def coin = args[0]
+
 print("Creating new ${coin} wallet ...")
-org.bitcoinj.core.Context.propagate(new org.bitcoinj.core.Context(blockchain.getNetworkParameters()));
-Wallet BTCwallet = new Wallet(blockchain,dir,seed)
+
+Context.propagate(new Context(params));
+bip47App = new BIP47AppKit(coin, params, dir, seed);
 println("done.")
+
+println("Payment code: " + bip47App.getPaymentCode());
 
 class Listener extends BlockchainDownloadProgressTracker{
 
@@ -48,18 +54,18 @@ class Listener extends BlockchainDownloadProgressTracker{
         return 42;
     }
 }
-BTCwallet.setBlockchainDownloadProgressTracker(new Listener())
+bip47App.setBlockchainDownloadProgressTracker(new Listener())
 
-BTCwallet.stop();
+bip47App.stop();
 print("Starting blockchain download ...")
-prevHeight = BTCwallet.vWallet.getLastBlockSeenHeight();
-BTCwallet.start(true)
+prevHeight = bip47App.vWallet.getLastBlockSeenHeight();
+bip47App.startBlockchainDownload()
 println("done.")
 
 while (true) {
-    lastHeight = BTCwallet.vWallet.getLastBlockSeenHeight();
+    lastHeight = bip47App.getvWallet().getLastBlockSeenHeight();
     print("\r")
-    print("Last height: ${lastHeight} Balance: ${BTCwallet.vWallet.getBalance()} Peers: ${BTCwallet.getConnectedPeers().size}\r")
+    print("Last height: ${lastHeight} Balance: ${bip47App.getvWallet().getBalance()} Peers: ${bip47App.getConnectedPeers().size}\r")
     Thread.sleep(200)
 }
 println("done")
